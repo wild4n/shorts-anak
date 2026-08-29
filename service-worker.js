61a30ab199bd62d6aa06c3c6c1f57a178cf6ba5c
wild4n <wildanmahesa@gmail.com>
2026-08-29 13:19:58 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'shorts-aman-v1';
+const SHELL_FILES = [
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  // Only handle our own shell files with cache-first.
+  // YouTube embeds and thumbnails always go to the network.
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => cached || fetch(event.request))
+    );
+  }
+});
